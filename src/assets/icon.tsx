024d11670eaac5df78b32ba326d836e38e6b29f2
EniolaAdemola <it.eniolaademola@gmail.com>
2025-02-12 18:06:31 -0800

    Add SVG icon components for header, play, about us, arrow, pencil, and one

---
+++ src/assets/icon.tsx
@@ -0,0 +1,184 @@
+import { IconProps } from "../@types/base-interface";
+
+export const HeaderIcon1: React.FC<IconProps> = ({
+  width,
+  height,
+  className,
+}) => {
+  return (
+    <svg
+      width={width || "500"}
+      height={height || "758"}
+      className={className}
+      viewBox="0 0 400 658"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M-162.61 217.95L-63.8698 214.638L-177.333 308.671L62.4616 215.531L-184.225 378.091L46.266 260.119L-215.747 460.054L318.207 181.98L-232.934 563.944L185.607 347.469L-37.3158 504.699L111.467 489.274"
+        stroke="url(#paint0_linear_13_88)"
+        stroke-width="48.0206"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_13_88"
+          x1="-154.332"
+          y1="453.952"
+          x2="209.124"
+          y2="260.519"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#30BFAB" />
+          <stop offset="0.48" stop-color="#CFFF4C" />
+          <stop offset="1" stop-color="#DAFF72" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+};
+
+export const HeaderIcon2: React.FC<IconProps> = ({
+  width,
+  height,
+  className,
+}) => {
+  return (
+    <svg
+      width={width || "336"}
+      height={height || "506"}
+      className={className}
+      viewBox="0 0 336 506"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M406.871 101.555C376.133 84.6666 343.447 158.443 312.72 141.534C293.87 131.161 304.906 97.0333 283.771 93.0165C263.612 89.185 258.264 117.181 238.253 123.505C183.267 140.881 172.872 -0.748756 120.477 39.1392C44.3835 97.0692 210.762 161.339 161.451 209.548C134.534 235.863 78.6108 197.193 71.6259 234.654C61.1213 290.99 170.688 248.471 188.27 299.809C200.028 334.141 153.256 369.885 178.496 394.459C207.899 423.085 234.077 348.692 274.986 347.055C347.938 344.136 339.116 511.411 407.037 480.24C475.119 448.994 363.734 341.861 413.099 282.747C437.044 254.073 497.255 275.646 496.808 238.291C496.334 198.686 424.576 232.008 402.206 201.177C379.971 170.533 439.417 119.436 406.871 101.555Z"
+        stroke="#C8FF32"
+        stroke-width="43.1611"
+        stroke-dasharray="1.8 1.8"
+      />
+    </svg>
+  );
+};
+
+export const PlayIcon: React.FC<IconProps> = ({ width, height, className }) => {
+  return (
+    <svg
+      width={width || "24"}
+      height={height || "24"}
+      className={className}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        opacity="0.5"
+        d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
+        stroke="#30BFAB"
+        stroke-width="1.5"
+      />
+      <path
+        d="M13.8876 9.9348C14.9625 10.8117 15.5 11.2501 15.5 12C15.5 12.7499 14.9625 13.1883 13.8876 14.0652C13.5909 14.3073 13.2966 14.5352 13.0261 14.7251C12.7888 14.8917 12.5201 15.064 12.2419 15.2332C11.1695 15.8853 10.6333 16.2114 10.1524 15.8504C9.6715 15.4894 9.62779 14.7336 9.54038 13.2222C9.51566 12.7947 9.5 12.3757 9.5 12C9.5 11.6243 9.51566 11.2053 9.54038 10.7778C9.62779 9.26636 9.6715 8.51061 10.1524 8.1496C10.6333 7.78859 11.1695 8.11466 12.2419 8.76679C12.5201 8.93597 12.7888 9.10831 13.0261 9.27492C13.2966 9.46483 13.5909 9.69274 13.8876 9.9348Z"
+        stroke="#30BFAB"
+        stroke-width="1.5"
+      />
+    </svg>
+  );
+};
+export const AboutUsIcon: React.FC<IconProps> = ({
+  width,
+  height,
+  className,
+}) => {
+  return (
+    <svg
+      width={width || "65"}
+      height={height || "62"}
+      className={className}
+      viewBox="0 0 65 62"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M39.6218 20.4186L36.7961 2L31.068 21.3249L9.87379 13.7721L24.2983 28.7215L3 41.8479L27.579 37.1599V59.558L38.4625 37.1599L62 46.2233L43.7948 28.7215L59.3963 16.064L39.6218 20.4186Z"
+        stroke="url(#paint0_linear_13_199)"
+        stroke-width="8.63371"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_13_199"
+          x1="32.5"
+          y1="2"
+          x2="32.5"
+          y2="59.558"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#DAFF72" />
+          <stop offset="1" stop-color="#30BFAB" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+};
+
+export const ArrowIcon: React.FC<IconProps> = ({
+  width,
+  height,
+  className,
+}) => {
+  return (
+    <svg
+      width={width || "13"}
+      height={height || "13"}
+      className={className}
+      viewBox="0 0 13 13"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M11.0037 3.41421L2.39712 12.0208L0.98291 10.6066L9.5895 2H2.00373V0H13.0037V11H11.0037V3.41421Z"
+        fill="white"
+      />
+    </svg>
+  );
+};
+export const PencilIcon: React.FC<IconProps> = ({
+  width,
+  height,
+  className,
+}) => {
+  return (
+    <svg
+      width={width || "267"}
+      height={height || "197"}
+      className={className}
+      viewBox="0 0 267 197"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M12.7564 37.2293L183.694 -121C199.439 -121 216.094 -115.204 231.187 -105.858M12.7564 37.2293L9.37821 113.132M12.7564 37.2293C34.2041 35.5824 58.4897 41.6169 81.2475 52.833M161.398 137.982L282 -40.8711C279.482 -52.4392 273.589 -64.0966 265.469 -74.8009M161.398 137.982L83.6989 163.508M161.398 137.982C156.457 120.948 146.079 104.616 132.445 90.2355M9.37821 113.132L6 189.035L83.6989 163.508M9.37821 113.132C9.37821 113.132 31.799 117.129 50.5924 129.868C69.3858 142.606 83.6989 163.508 83.6989 163.508M132.445 90.2355L265.469 -74.8009M132.445 90.2355C118.127 75.1327 100.218 62.1826 81.2475 52.833M265.469 -74.8009C256.322 -86.859 244.348 -97.7078 231.187 -105.858M81.2475 52.833L231.187 -105.858"
+        stroke="#DAFF72"
+        stroke-width="10.6358"
+      />
+    </svg>
+  );
+};
+export const OneIcon: React.FC<IconProps> = ({ width, height, className }) => {
+  return (
+    <svg
+      width={width || "44"}
+      height={height || "110"}
+      className={className}
+      viewBox="0 0 44 110"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M3 50.6718L40.2585 3L19.5217 108.449"
+        stroke="#DAFF72"
+        stroke-width="7.35688"
+      />
+    </svg>
+  );
+};
