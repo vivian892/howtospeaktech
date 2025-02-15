803700d51e24456c634fe33f3a2a63ebc36aab35
EniolaAdemola <it.eniolaademola@gmail.com>
2025-02-14 20:25:26 -0800

    feat: add HowItWorks component to explain platform functionality

---
+++ src/components/how-it-works/HowItWorks.tsx
@@ -0,0 +1,10 @@
+const HowItWorks = () => {
+  return (
+    <section className="bg-[#0169ff]">
+      HowItWorks
+      <p>How Schoolyft Works: Simple, Smart, and Seamless</p>
+    </section>
+  );
+};
+
+export default HowItWorks;
