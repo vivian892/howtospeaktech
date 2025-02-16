7f5fec7c2c4fcb87af48c22b87bfe7cd6ba72537
EniolaAdemola <it.eniolaademola@gmail.com>
2025-02-16 10:33:48 -0800

    feat: update Feature component to include Schools, Teachers, and Parents tabs

--- src/components/feature/Feature.tsx
+++ src/components/feature/Feature.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import About from "../about/About";
-import AboutUsButtom from "../about/AboutUsButtom";
+import Schools from "./Schools";
+import Teachers from "./Teachers";
+import Parents from "./Parents";
 
 const Feature = () => {
   const [activeTab, setActiveTab] = useState("Schools");
@@ -8,28 +9,28 @@ const Feature = () => {
   const renderContent = () => {
     switch (activeTab) {
       case "Schools":
-        return <About />;
+        return <Schools />;
       case "Teachers":
-        return <AboutUsButtom />;
-      case "Parentss":
-        return <AboutUsButtom />;
+        return <Teachers />;
+      case "Parents":
+        return <Parents />;
       default:
-        return <About />;
+        return <Schools />;
     }
   };
   return (
     <section>
       <div className="text-black text-center">
-        <h2>Feature</h2>
-        <p>
-          Tools for Schools, Teachers, and Parents—All in One Place. Designed to
-          Make Education Smarter, Smoother, and More Connected
+        <h2 className="text-4xl font-bold mb-6">Feature</h2>
+        <p className="text-xl leading-relaxed">
+          wTools for Schools, Teachers, and Parents—All in One Place. Designed
+          to Make Education Smarter, Smoother, and More Connected
         </p>
       </div>
       <section className="flex flex-col items-center w-full">
         {/* Tabs Navigation */}
         <ul className="flex justify-center gap-6 px-6 py-4 w-full max-w-6xl">
-          {["Schools", "Teachers", "Parentss"].map((tab) => (
+          {["Schools", "Teachers", "Parents"].map((tab) => (
             <li
               key={tab}
               onClick={() => setActiveTab(tab)}
