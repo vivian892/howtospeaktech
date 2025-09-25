b3a0906edd73751899ab8dd536f8b402f36fc309
rigAITe <aitboy18@gmail.com>
2025-09-25 22:29:08 +0100

    fix: i added a comment just for you to see

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Header from "./pages/homepage/Header";
 import Navbar from "./pages/homepage/Navbar";
-import "./App.css";
+import "./App.css"; // app.css import fix
 import AboutUs from "./pages/homepage/AboutUs";
 import Feature from "./components/feature/Feature";
 import HowItWorks from "./components/how-it-works/HowItWorks";
